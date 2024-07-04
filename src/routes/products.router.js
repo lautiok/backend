97d809392ff9c80d07fc2a13b3f2717b58db6fe9
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-07-04 16:34:36 -0300

    err-prod-[ERR_MODULE_NOT_FOUND]

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,58 +1,44 @@
-import CustomRouter from "./custom.router.js";
-import ProductsController from "../controllers/products.controller.js";
+import CustomRouter from './custom.router.js';
+import ProductsController from '../controllers/products.controller.js';
 
 export default class ProductsRouter extends CustomRouter {
-  static #instance;
+    static #instance;
 
-  constructor() {
-    super();
-  }
-
-  static getInstance() {
-    if (!this.#instance) {
-      this.#instance = new ProductsRouter();
+    constructor() {
+        super();
     }
-    return this.#instance;
-  }
 
-  init() {
-    this.get("/", ["ALL"], ProductsController.getProducts);
-
-    this.get("/:pid", ["ALL"], ProductsController.getProductById);
-
-    this.post(
-      "/",
-      ["PREMIUM", "ADMIN"],
-      this.validateProductFields,
-      ProductsController.createProduct
-    );
-
-    this.put(
-      "/:pid",
-      ["PREMIUM", "ADMIN"],
-      this.validateProductFields,
-      ProductsController.updateProduct
-    );
-
-    this.delete(
-      "/:pid",
-      ["PREMIUM", "ADMIN"],
-      ProductsController.deleteProduct
-    );
-  }
-
-  validateProductFields(req, res, next) {
-    const { title, code, price } = req.body;
-    if (!title || !code || !price) {
-      req.logger.warning("Los campos título, código y precio son obligatorios");
-      return res.sendUserError(
-        "Los campos título, código y precio son obligatorios"
-      );
+    static getInstance() {
+        if (!this.#instance) {
+            this.#instance = new ProductsRouter();
+        }
+        return this.#instance;
     }
-    if (isNaN(price) || price < 0) {
-      req.logger.warning("El precio debe ser un número positivo");
-      return res.sendUserError("El precio debe ser un número positivo");
+
+    init() {
+        this.get('/', ['ALL'], ProductsController.getProducts);
+
+        this.get('/:pid', ['ALL'], ProductsController.getProductById);
+
+        this.post('/', ['PREMIUM', 'ADMIN'], this.validateProductFields, ProductsController.createProduct);
+
+        this.put('/:pid', ['PREMIUM', 'ADMIN'], this.validateProductFields, ProductsController.updateProduct);
+
+        this.delete('/:pid', ['PREMIUM', 'ADMIN'], ProductsController.deleteProduct);
     }
-    next();
-  }
+
+    validateProductFields(req, res, next) {
+        const { title, code, price } = req.body;
+        // Se valida que los campos título, código y precio sean obligatorios
+        if (!title || !code || !price) {
+            req.logger.warning('Los campos título, código y precio son obligatorios');
+            return res.sendUserError('Los campos título, código y precio son obligatorios');
+        }
+        // Se valida que el precio sea un número positivo
+        if (isNaN(price) || price < 0) {
+            req.logger.warning('El precio debe ser un número positivo');
+            return res.sendUserError('El precio debe ser un número positivo');
+        }
+        next();
+    }
 }
